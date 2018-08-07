dd1be74ff71249ec24be60e05c221701f238c11c
Ryan Pfeifer <ryanpfeifer1111@gmail.com>
2018-08-06 21:44:45 -0500

    added several different color palettes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var audio, canvas, context, audioctx, analyser, oscillator, freqArr, barHeight, source;
+var rand = 0;
+var pastIndex = 900;
 var WIDTH = 1024;
 var HEIGHT = 300;
 var INTERVAL = 128;
@@ -52,6 +54,7 @@ audioFile.onchange = function(){ //plays the user's uploaded audio file when it 
         source = audioctx.createMediaElementSource(audio);    
         source.connect(analyser);
         source.connect(audioctx.destination); //from online help
+        rand = Math.round(Math.random() * 6); //random color palete if you switch songs
         //
         audio.play();
     }
@@ -81,14 +84,26 @@ function draw(){
         context.clearRect(0,0,WIDTH, HEIGHT);
         analyser.getByteFrequencyData(freqArr);
         //analyser.getByteTimeDomainData(freqArr);
-        console.log(freqArr);
+        //console.log(freqArr);
         for(var i = 0; i < INTERVAL; i++){
             //barHeight = (Math.random() * HEIGHT);
+            /*
             var max = maxIndex(freqArr);
             //console.log(max);
+            if(Math.abs(max - pastIndex) >= 300){
+                pastIndex = max;
+            }
+            else{
+                max = pastIndex;
+            }   //not sure if this really give a great effect
+            //console.log(max);
+            */
+            max = 900; //default placeholder
             var num = (max - INTERVAL*Math.floor(max/INTERVAL)) + (Math.floor(max/INTERVAL)*i);
 
-            barHeight = (freqArr[num] * (3/2)) + 2; //for frequency
+            barHeight = (freqArr[num] * (4/3)) + 2; //exaggerate the bar
+
+            //for frequency
             //barHeight = (Math.abs(freqArr[i*(WIDTH/INTERVAL)]) - 120) * 2 + 1; //for time 
             
 
@@ -105,10 +120,29 @@ function draw(){
             b = 0;
             }
 
-            context.fillStyle = "rgb(" + r + "," + g + "," + b + ")"; //rgb color cycle 
-            //context.save();
+            //I SHOULD MAKE AN OPTION TO TOGGLE BETWEEN DIFFERENT COLOR PALETTES
+            if(rand == 0){
+                context.fillStyle = "rgb(" + r + "," + g + "," + b + ")"; //ORIGINAL rgb color cycle 
+            }
+            else if(rand == 1){
+                context.fillStyle = "rgb(" + ((2/3)*(barHeight)) + "," + (0*(barHeight)) + "," + (0*(barHeight)) + ")"; //red color gradient depending on height of bar
+            }
+            else if(rand == 2){
+                context.fillStyle = "rgb(" + (1*(barHeight)) + "," + (.6*(barHeight)) + "," + (0*(barHeight)) + ")"; //orange color gradient depending on height of bar
+            }
+            else if(rand == 3){
+                context.fillStyle = "rgb(" + (.95*(barHeight)) + "," + (.85*(barHeight)) + "," + (0*(barHeight)) + ")"; //yellow color gradient depending on height of bar
+            }
+            else if(rand == 4){
+                context.fillStyle = "rgb(" + (0*(barHeight)) + "," + ((2/3)*(barHeight)) + "," + (0*(barHeight)) + ")"; //green color gradient depending on height of bar
+            }
+            else if(rand == 5){
+                context.fillStyle = "rgb(" + (.58*(barHeight/10)) + "," + (0*(barHeight)) + "," + (1*(barHeight)) + ")"; //blue color gradient depending on height of bar
+            }
+            else{
+                context.fillStyle = "rgb(" + (1*(barHeight)) + "," + (0*(barHeight)) + "," + (1*(barHeight)) + ")"; //purple color gradient depending on height of bar
+            }
 
-            //context.fillStyle = "rgb(" + 50 + "," + 50 + "," + (100 + (100 - barHeight)) + ")"; //blue color gradient depending on height of bar
 
             //context.restore();
             context.fillRect(x, HEIGHT - barHeight, (WIDTH/INTERVAL) - 1 , barHeight);
